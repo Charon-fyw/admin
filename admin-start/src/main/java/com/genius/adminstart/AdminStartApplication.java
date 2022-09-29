d715bb1f2b0e95c57943740e9b15c1313b430af6
stink <stinky_pig@163.com>
2022-09-29 11:07:28 +0800

    init

---
+++ admin-start/src/main/java/com/genius/adminstart/AdminStartApplication.java
@@ -0,0 +1,13 @@
+package com.genius.adminstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminStartApplication.class, args);
+	}
+
+}
